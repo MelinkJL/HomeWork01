4844d6591d90664767a169653519a496dc33644a
Thomas C. Willard <carywillard@yahoo.com>
2012-09-04 01:26:07 -0400

    Tint and Gradient

--- HomeWork01/src/HomeWork01App.cpp
+++ HomeWork01/src/HomeWork01App.cpp
@@ -37,99 +37,27 @@ class HomeWork01App : public AppBasic {
 	void mouseDown( MouseEvent event );	
 	void update();
 	void draw();
+	void presets(Settings* settings);
 
 private:
 	static const int kWinWidth = 900;
 	static const int kWinHeight = 900;
 	static const int kSurfaceSize=1024;
-
-	// Strobe Variables & Constants
-	int currentFrame;
-	int red;
-	int green;
-	int blue;
-	double redStrobePeriod;
-	double greenStrobePeriod;
-	double blueStrobePeriod;
-	int strobeDivisor;
-	int redAmp;
-	int greenAmp;
-	int blueAmp;
-
-
 	Surface* mySurface_;
 	uint8_t* myPixels;
-	void blackOutWindow(uint8_t* pixels);
-	double pi();
-	void strobe(int currentFrame);
 	void drawCircle(uint8_t* pixels,int x, int y, int r, Color8u c);
 	void drawSquare(uint8_t* pixels,int x, int y, int r, Color8u c);
-	void drawRectangle(uint8_t* pixels,int x, int y, int width, int height, int lineThickness, Color8u c);
 	void drawDiamond(uint8_t* pixels,int x, int y, int r, Color8u c);
+	void drawRectangle(uint8_t* pixels,int center_x, int center_y, int rx, int ry, Color8u c);
+	void blackOutWindow(uint8_t* pixels);
+	void gradient(uint8_t* pixels, Color8u c);
+	void tint(uint8_t* pixels, Color8u t);
 };
 
-void HomeWork01App::setup()
-{
-	// Strobe Variables & Constants
-	currentFrame = 1;
-	//PI=3.141592;
-	red = 127;
-	green = 127;
-	blue = 127;
-	redStrobePeriod = 1;
-	greenStrobePeriod = 2;
-	blueStrobePeriod = 4;
-	strobeDivisor = 20;
-	redAmp = 255;
-	greenAmp = 255;
-	blueAmp = 255;
-
-	// Initialize mySurface_
-	mySurface_ = new Surface(kSurfaceSize,kSurfaceSize,false);
-	myPixels = (*mySurface_).getData();
-	//blackOutWindow(myPixels);
-	strobe(currentFrame);
-	drawCircle(myPixels,300, 350, 5, Color(0,0,0));
-	drawSquare(myPixels,300, 350, 100, Color(0,0,0));
-	drawDiamond(myPixels,200, 250, 100, Color(0,0,0));
-	drawRectangle(myPixels,300,100,50,100,5,Color(0,0,0));
-	
+void HomeWork01App::presets(Settings* settings) {
+	(*settings).setWindowSize(kWinWidth,kWinHeight);
+	(*settings).setResizable(false);
 }
-
-void HomeWork01App::blackOutWindow(uint8_t* pixels)
-{
-	Color8u c = Color(0,0,0);
-
-	for(int y=0; y<kWinWidth; y++) {
-		for(int x=0; x<kWinWidth; x++) {
-			int offset = 3*(x + y*kSurfaceSize);
-			pixels[offset] = pixels[offset] + c.r; //Red
-			pixels[offset+1] = pixels[offset+1] + c.g; //Green
-			pixels[offset+2] = pixels[offset+2] + c.b; // Blue
-		}
-	}
-}
-
-double HomeWork01App::pi()
-{
-	return 3.141592;
-}
-
-/*
-This function is supposed to change the integer variables that make up
-a Color according the number of frames since the app started.
-*/
-void HomeWork01App::strobe(int currentFrame)
-{
-double frame =  (double)currentFrame/strobeDivisor;
-
-red = (int)(redAmp*sin(frame*pi()/redStrobePeriod));
-green = (int)(greenAmp*sin(frame*pi()/greenStrobePeriod));
-blue = (int)(blueAmp*sin(frame*pi()/blueStrobePeriod));
-
-}
-
-
 /*
 Right now, this is almost a verbatim implementation of Dr. Brinkman's drawRings method from his HW01 project.
 I am analyzing it so that I can understand it well enough to improvise later.
@@ -147,13 +75,13 @@ void HomeWork01App::drawCircle(uint8_t* pixels,int center_x, int center_y, int r
 
 			int distanceFromCenter = (int)sqrt((double)((x-center_x)*(x-center_x) + (y-center_y)*(y-center_y)));
 			if(distanceFromCenter <= r){
-				//if((distanceFromCenter/7)%2 == 1 ){  //...(distanceFromCenter/7)
+				if((distanceFromCenter/7)%2 == 1 ){  //...(distanceFromCenter/7)
 						int offset = 3*(x + y*kSurfaceSize);
 						//Dr. Brinkman: "By blending the colors I get a semi-transparent effect"
 						pixels[offset] = pixels[offset]/2 + c.r/2; //Red
 						pixels[offset+1] = pixels[offset+1]/2 + c.g/2; //Green
 						pixels[offset+2] = pixels[offset+2]/2 + c.b/2; // Blue
-					//}
+					}
 				}
 			}	
 		}
@@ -180,36 +108,6 @@ void HomeWork01App::drawSquare(uint8_t* pixels,int center_x, int center_y, int r
 	}
 }
 
-void HomeWork01App::drawRectangle(uint8_t* pixels,int center_x, int center_y, int width, int height, int lineThickness, Color8u c)
-{
-	if(width <= 0) return;
-	if(height <= 0) return;
-
-	lineThickness = lineThickness/2; // Center the width of the line on the true line
-
-	for(int y=center_y-(height/2)-lineThickness;y<=center_y+(height/2)+lineThickness; y++){
-		for(int x=center_x-(width/2)-lineThickness;x<=center_x+(width/2)+lineThickness; x++){
-
-			if(
-				  (x <= center_x-(width/2)+lineThickness)&&(x >= center_x-(width/2)-lineThickness)
-				||(x <= center_x+(width/2)+lineThickness)&&(x >= center_x+(width/2)-lineThickness)
-				||
-				(y <= center_y-(height/2)+lineThickness)&&(y >= center_y-(height/2)-lineThickness)
-				||(y <= center_y+(height/2)+lineThickness)&&(y >= center_y+(height/2)-lineThickness)
-				) {
-
-			if(y < 0 || x < 0 || x >= kWinWidth || y >= kWinHeight) continue;
-
-			int offset = 3*(x + y*kSurfaceSize); // Why not (y+x*kSurfaceSize)?
-			pixels[offset] = pixels[offset]/2 + c.r/2; //Red
-			pixels[offset+1] = pixels[offset+1]/2 + c.g/2; //Green
-			pixels[offset+2] = pixels[offset+2]/2 + c.b/2; // Blue
-			}
-	}
-	}
-}
-
-
 // Let's rotate that square by pi/4
 void HomeWork01App::drawDiamond(uint8_t* pixels,int center_x, int center_y, int r, Color8u c)
 {
@@ -219,28 +117,137 @@ void HomeWork01App::drawDiamond(uint8_t* pixels,int center_x, int center_y, int 
 	if(r <= 0) return;
 
 	for(int y=center_y-yRad;y<=center_y+yRad; y++){
+		//for(int x=center_x;x<=center_x+xRad; x++){
 			int xLeft=center_x-xRad;
 			int xRight=center_x+xRad;
 
 			if(y < 0 || xLeft < 0 || xRight < 0 || xLeft >= kWinWidth || xRight >= kWinWidth || y >= kWinHeight) continue;
 
+			//if((x == center_x-xRad)||(x == center_x+xRad)) {
+
 			int indeces = 3*(xLeft + y*kSurfaceSize);
-			pixels[indeces] = pixels[indeces]/2+c.r/2;
-			pixels[indeces] = pixels[indeces+1]/2+c.g/2;
-			pixels[indeces] = pixels[indeces+2]/2+c.b/2;
+			pixels[indeces] = pixels[indeces]+c.r;
+			pixels[indeces] = pixels[indeces+1]+c.g;
+			pixels[indeces] = pixels[indeces+2]+c.b;
 
 			indeces = 3*(xRight + y*kSurfaceSize);
-			pixels[indeces] = pixels[indeces]/2+c.r/2;
-			pixels[indeces] = pixels[indeces+1]/2+c.g/2;
-			pixels[indeces] = pixels[indeces+2]/2+c.b/2;
+			pixels[indeces] = pixels[indeces]+c.r;
+			pixels[indeces] = pixels[indeces+1]+c.g;
+			pixels[indeces] = pixels[indeces+2]+c.b;
 
 
+			//}
 			if(y<center_y) {
 			xRad--;
 			} else if(y>=center_y) {
 				xRad++;
+			}
+		//}
+	}
+}
+
+// Now we'll extend that square into a rectangle
+void HomeWork01App::drawRectangle(uint8_t* pixels,int center_x, int center_y, int rx, int ry, Color8u c)
+{
+	if(rx <= 0) return;
+	if(ry <= 0) return;
+
+	for(int y=center_y-ry;y<=center_y+ry; y++){
+		for(int x=center_x-rx;x<=center_x+rx; x++){
+			
+			if(y < 0 || x < 0 || x >= kWinWidth || y >= kWinHeight) continue;
+
+			if((x == center_x-rx)||(x == center_x+rx)||(y == center_y-ry)||(y == center_y+ry)) {
+
+			int indeces = 3*(x + y*kSurfaceSize); // Why not (y+x*kSurfaceSize)?
+			pixels[indeces] = pixels[indeces]+c.r;
+			pixels[indeces] = pixels[indeces+1]+c.g;
+			pixels[indeces] = pixels[indeces+2]+c.b;
+			}
 		}
 	}
+}
+
+void HomeWork01App::gradient(uint8_t* pixels, Color8u c)
+{
+	//Color8u c = Color8u(200,200,200);
+	int count = 0;
+	int colorAdd = 0;
+	bool flag = false;
+
+	for(int y=0; y<kSurfaceSize; y++) {
+		for(int x=0; x<kSurfaceSize; x++) {
+			count = x;
+
+			if(count==((int)(255/kSurfaceSize))) {
+				count = 0;
+				
+				if(colorAdd == 255) {
+					flag = true;
+				}
+				if(colorAdd == 0) {
+					flag = false;
+				}
+
+				if(flag) {
+				colorAdd--;
+				} else {
+					colorAdd++;
+				}
+				c.r = colorAdd;
+				c.g = colorAdd;
+				c.b = colorAdd;
+			}
+
+			int offset = 3*(x + y*kSurfaceSize);
+			pixels[offset] = c.r; //Red
+			pixels[offset+1] = c.g; //Green
+			pixels[offset+2] = c.b; // Blue
+
+		}
+	}
+}
+
+
+void HomeWork01App::tint(uint8_t* pixels, Color8u t)
+{
+
+	for(int y=0; y<kSurfaceSize; y++) {
+		for(int x=0; x<kSurfaceSize; x++) {
+			int offset = 3*(x + y*kSurfaceSize);
+			
+			if((pixels[offset] + t.r) > 255) {
+			pixels[offset] = 255;
+			} else {
+			pixels[offset] = pixels[offset] + t.r;
+			}
+			if((pixels[offset+1] + t.g) > 255) {
+			pixels[offset+1] = 255;
+			} else {
+			pixels[offset+1] = pixels[offset+1] + t.g;
+			}
+			if((pixels[offset+2] + t.b) > 255) {
+			pixels[offset+2] = 255;
+			} else {
+			pixels[offset+2] = pixels[offset+2] + t.b;
+			}
+		}
+	}
+}
+
+void HomeWork01App::setup()
+{
+	// Initialize mySurface_
+	mySurface_ = new Surface(kSurfaceSize,kSurfaceSize,false);
+	myPixels = (*mySurface_).getData();
+	//blackOutWindow(myPixels);
+	gradient(myPixels, Color8u(0,0,0));
+	//drawCircle(myPixels,300, 350, 100, Color8u(0,180,0));
+	drawSquare(myPixels,300, 350, 100, Color8u(180,180,180));
+	drawDiamond(myPixels,200, 250, 100, Color8u(180,255,180));
+	drawRectangle(myPixels,300, 350, 100, 200, Color8u(180,180,180));
+	tint(myPixels, Color8u(0,50,0));
+	
 }
 
 void HomeWork01App::mouseDown( MouseEvent event )
@@ -249,25 +256,12 @@ void HomeWork01App::mouseDown( MouseEvent event )
 
 void HomeWork01App::update()
 {
-	//blackOutWindow(myPixels);
-	currentFrame++;
-	strobe(currentFrame);
-	drawDiamond(myPixels,200, 250, 100, Color(0,0,0));
-
-	drawCircle(myPixels,300, 350, 100, Color(0,0,0));
-	//drawSquare(myPixels,300, 350, 100, Color(0,0,0));
-	drawRectangle(myPixels,300,100,50,100,5,Color(0,0,0));
-
-
-	//Get our array of pixel information
-	//uint8_t* dataArray = (*mySurface_).getData();
-
 }
 
 void HomeWork01App::draw()
 {
 	//gl::Texture myTexture = gl::Texture( myPixels);
-	gl::clear(Color(1.0,1.0,1.0));
+	gl::clear(Color8u(0,0,0));
 	gl::draw(*mySurface_);
 }
 
